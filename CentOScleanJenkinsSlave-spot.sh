7ad143a806ff0bd3f761724e6efe426adf26ebd9
MAX <max@biodatomics.com>
2013-06-22 22:49:12 -0300

    Update CentOScleanJenkinsSlave-spot.sh

--- CentOScleanJenkinsSlave-spot.sh
+++ CentOScleanJenkinsSlave-spot.sh
@@ -3,8 +3,10 @@
 #sudo apt-get install ec2-api-tools
 
 #Jenkins starts this script from root "/". we need to fix it
-# cd
+cd
+ls -lahs 
 
+exit
 #ami for CentOS 6.4 with only root disk and cloud-init installed
 #CentOS6.4 with cloud init on EBS storage : ami-1064f120
 #CentOS6.4 with all packages for Ambari building: ami-47be2f77
