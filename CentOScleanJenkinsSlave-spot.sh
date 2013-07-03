a3dcf9e9f97c9c5d74a34f13faaabfaf52444ca7
rasul <orasul@mail.ru>
2013-07-03 20:09:56 +0400

    Added github fingerprint for ssh connections.

--- CentOScleanJenkinsSlave-spot.sh
+++ CentOScleanJenkinsSlave-spot.sh
@@ -65,6 +65,10 @@ yum -y install curl
 
 rvm install jruby-1.7.3
 
+cat > /home/ec2-user/.ssh/known_hosts << EOF
+github.com,204.232.175.90 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+EOF
+
 EOF
 
 SIR_REQUEST_TMP=`ec2-request-spot-instances -k $KEY_NAME --region $REGION $AMI -n $NUM_INSTANCES -b $MAPPING -p $PRICE -t $TYPE \
